b50057f89eb40e7c000df3e0f61fd283cbd974b0
William <wjt2001live@gmail.com>
2021-04-23 23:19:31 +0100

    added some stuff

--- DeaconFirmware.ino
+++ DeaconFirmware.ino
@@ -1,25 +1,62 @@
 #include <NeoSWSerial.h>
 #include "Timer.h"
+#include "millisDelay.h"
+// OK+DISISOK+DISC:4C000215:2G234454CF6D5A0FADF2F4911BA9FFA7:00000001AC:0CF3EE041CCE:-052OK+DISC:00000000:00000000000000000000000000000000:0000000000:B9782E08068C:-071OK+DISCE
 
-NeoSWSerial bluetoothModule{ 2, 3 };
+struct Reading
+{
+  unsigned long TS;
+  String UUID;
+  String RSSI;
+};
+
+NeoSWSerial bt{ 2, 3 };
 Timer timer{};
+millisDelay loopDelay;
 
-// currently arbitrary number for testing
-int numReadings{ 10 };
+int numReadings{ 10 };  //
+int scanInterval{ 100 }; // time in ms between scans.
 
-// current format: 'timestamp,uuid,rssi'
 String readLineFromSD(int i);
+
+
+void setupBT();
+
+// returns result if success. returns blank string if failure.
+String scan();
+void writeReading(const Reading& reading); 
+
 
 void setup()
 {
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(9600);
+  bt.begin(9600);
+  loopDelay.start(50);
+  setupBT();
 }
 
 void loop()
 {
-  Serial.write(String(timer.getTime()).c_str());
-  Serial.write('\n');
-  delay(1000);
+  if (loopDelay.justFinished())
+  {
+    String rawData{ scan() };
+
+    // iterates over string looking for '-' characters, and assigns the index of
+    // these in the string to i.
+    for(int i{rawData.indexOf('-')};
+      i < rawData.length();
+      i = rawData.indexOf('-', i + 1))
+    {
+      Reading reading;
+      reading.TS = timer.getTime();
+      reading.UUID = rawData.substring(i /* + something*/, i /* + something*/);
+      reading.RSSI = rawData.substring(i /* + something*/, i /* + something*/);
+      writeReading(reading);
+    }
+    
+    loopDelay.start(scanInterval);
+  }
 }
 
 // Called whenever arduino recieves something from serial port.
@@ -27,11 +64,8 @@ void loop()
 void serialEvent()
 {
   String msg{ Serial.readStringUntil('\n') };
-  
-  //if(!msg)
-  // return;
-  
-  if(msg == "sync")
+
+  if (msg == "sync")
   {
     // write timestamp
     Serial.write("ts:");
@@ -39,7 +73,7 @@ void serialEvent()
     Serial.write('\n');
 
     // write all readings
-    for(int i{}; i < numReadings; ++i)
+    for (int i{}; i < numReadings; ++i)
       Serial.write(readLineFromSD(i).c_str());
 
     // notify completion and reset timer
@@ -52,7 +86,32 @@ void serialEvent()
   }
 }
 
+String scan()
+{
+  millisDelay timeout;
+  bt.write("AT+DISI?");
+  timeout.start(5000);
+  // loop runs given that the 5 second timout hasn't finished,
+  // in which case it gives up and returns an empty string
+  while(!timeout.justFinished())
+    if(bt.available())
+      return bt.readStringUntil('\n');
+  return "";
+}
+
+void setupBT()
+{
+  // this needs to setup the bluetooth module
+  // as a beacon and as something that can listen for beacons.
+}
+
+void writeReading(const Reading& reading)
+{
+  // this needs to write a line to the sd card with each part of reading
+}
+
 String readLineFromSD(int i)
 {
+  // this needs to read a line from the sd card, with a newline character at the end.
   return "FFFFFFFF,db559330acb9442398e3248894e62ed1,069\n";
 }
