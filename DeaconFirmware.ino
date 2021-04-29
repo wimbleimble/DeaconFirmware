9983dce75ee893bc1b9a472777ce8da22ae1b9aa
William <wjt2001live@gmail.com>
2021-04-29 16:03:27 +0100

    All systems functional except for actual data collection, which is currently borked because i ran out of sram on the uno.

--- DeaconFirmware.ino
+++ DeaconFirmware.ino
@@ -2,7 +2,6 @@
 #include <SPI.h>
 #include <SD.h>
 #include "millisDelay.h"
-
 #include "Timer.h"
 
 namespace sd
@@ -13,58 +12,49 @@ namespace sd
   void wipe();
 }
 
-void setupBT();
 String scan();
-
 
 NeoSWSerial bt{ 2, 3 };
 Timer timer{};
 millisDelay loopDelay;
 
 // you could probably do this without keeping track of this, but honestly this is easiest
-int numReadings{ 0 };
-int scanInterval{ 100 };
-String UUID{ "ac5fe330acb93642398e7348894e62ed" };
-
+constexpr int numReadings{ 5 };
+constexpr int scanInterval{ 100 };
+constexpr char UUID[]{ "ac5fe330acb93642398e7348894e62ed" };
 
 void setup()
 {
   digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(9600);
   bt.begin(9600);
+  sd::init();
   loopDelay.start(50);
-//  setupBT();
-}
-
-void setupBT()
-{
-  // this needs to setup the bluetooth module
-  // as a beacon and as something that can listen for beacons.
 }
 
 void loop()
 {
   if (loopDelay.justFinished())
   {
-    String rawData{ scan() };
-
-    // iterates over string looking for '-' characters, and assigns the index of
-    // these in the string to i.
-    for(int i{rawData.indexOf('-')};
-      i < rawData.length();
-      i = rawData.indexOf('-', i + 1))
-    {
-      if(i >= 57) // prevents out of bounds index
-      {
-        ++numReadings;
-        sd::writeReading(
-          timer.getTime(),
-          rawData.substring(i - 57, i - 26),
-          rawData.substring(i + 1, i + 3)
-        );
-      }
-    }
-    
+//    String rawData{ scan() };
+//
+//    // iterates over string looking for '-' characters, and assigns the index of
+//    // these in the string to i.
+//    for(int i{rawData.indexOf('-')};
+//      i < rawData.length();
+//      i = rawData.indexOf('-', i + 1))
+//    {
+//      if(i >= 57) // prevents out of bounds index
+//      {
+//        ++numReadings;
+//        sd::writeReading(
+//          timer.getTime(),
+//          rawData.substring(i - 57, i - 26),
+//          rawData.substring(i + 1, i + 3)
+//        );
+//      }
+//    }
+//    
     loopDelay.start(scanInterval);
   }
 }
@@ -74,12 +64,21 @@ String scan()
   millisDelay timeout;
   bt.write("AT+DISI?");
   timeout.start(5000);
-  // loop runs given that the 5 second timout hasn't finished,
-  // in which case it gives up and returns an empty string
+  // pretty sure this doesn't work. hard to figure out when 
+  // we keep getting a heap/stack collision tho
   while(!timeout.justFinished())
     if(bt.available())
       return bt.readStringUntil('\n');
   return "";
+}
+
+void sd::init()
+{
+  if (!SD.begin(10))
+  {
+    Serial.println("Card failed, or not present");
+    while (1);
+  }
 }
 
 void sd::writeReading(unsigned long timestamp, const String& uuid, const String& rssi)
@@ -137,7 +136,7 @@ void serialEvent()
     
     // write beacon's uuid
     Serial.write("uuid:");
-    Serial.write(UUID.c_str());
+    Serial.write(UUID);
     Serial.write('\n');
 
     // write all readings
@@ -147,11 +146,12 @@ void serialEvent()
     // notify completion and reset timer
     Serial.write("done\n");
     timer.reset();
+    // sd::wipe();
   }
   else if (msg == "uuid")
   {
     Serial.write("uuid:");
-    Serial.write(UUID.c_str());
+    Serial.write(UUID);
     Serial.write('\n');
   }
   else
